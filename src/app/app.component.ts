109b8bf39dd2a6f7165d1e5f9a887c3194dd1483
Ashwani Garg <akgarg007@gmail.com>
2018-07-03 15:11:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ToDo Tasks List';
+  taskList= [];
+  task = '';
+
+  addTask() {
+    if(this.task !== '')
+    {
+      this.taskList.push(this.task);
+      this.task = '';
+    }
+  }
+
+  // splice function removes element from the array of the given index
+  removeTask(index){
+    this.taskList.splice(index , 1);
+  }
+
+
 }
